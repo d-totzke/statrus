394a61a615435d696d35942253291237495833d8
dylan <drtotzke@uwaterloo.ca>
2020-08-10 08:49:21 -0400

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.create([{name: 'Dylan', email: 'dylan.totzke@uwaterloo.ca', password: 'pass.me', password_confirmation: 'pass.me'}])
